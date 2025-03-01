4f64a62eecf9e14d64fde7707c65e3fc69b9f9a3
NidzaX <59400424+NidzaX@users.noreply.github.com>
2025-03-01 10:53:13 +0100

    Setting up React Query
    Fetching Cabin Data
    Mutations Deleting a Cabin
    Displaying Toasts(Notifications)
    Creating a New Cabin,
    Handling Form Errors,
    Uploading Images to Supabase

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://dfkvlboygwiqwxceyoye.supabase.co";
+export const supabaseUrl = "https://dfkvlboygwiqwxceyoye.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRma3ZsYm95Z3dpcXd4Y2V5b3llIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA2NDcxOTcsImV4cCI6MjA1NjIyMzE5N30.x_KAZhzcDrqeo0Ci_foUFnZ9sXaPk0JqWQnRmR1KF8w";
 const supabase = createClient(supabaseUrl, supabaseKey);
