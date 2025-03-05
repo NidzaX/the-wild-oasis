a74b0f236b43607acbbc74150e052d062b416e63
NidzaX <59400424+NidzaX@users.noreply.github.com>
2025-03-05 08:15:52 +0100

    Building the Bookings Table
    Uploading Sample Data
    API-Side Filtering
    API-Side Sorting

--- src/ui/Empty.jsx
+++ src/ui/Empty.jsx
@@ -1,5 +1,5 @@
-function Empty({ resource }) {
-  return <p>No {resource} could be found.</p>;
+function Empty({ resourceNmae }) {
+  return <p>No {resourceNmae} could be found.</p>;
 }
 
 export default Empty;
