7c1305e741dad208e5c2091d6e37fbe209667092
NidzaX <59400424+NidzaX@users.noreply.github.com>
2025-03-07 07:32:45 +0100

    Implementing Authorization Protecting Routes

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,17 +1,18 @@
 import toast from "react-hot-toast";
 
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { login as loginApi } from "../../services/apiAuth";
 import { useNavigate } from "react-router-dom";
 
 export function useLogin() {
+  const queryClient = useQueryClient();
   const navigate = useNavigate();
 
   const { mutate: login, isLoading } = useMutation({
     mutationFn: ({ email, password }) => loginApi({ email, password }),
     onSuccess: (user) => {
-      console.log(user);
-      navigate("/dashboard");
+      queryClient.setQueriesData(["user"], user);
+      setTimeout(() => navigate("/dashboard"));
     },
     onError: (err) => {
       toast.error("Provided email or password are incorrect");
