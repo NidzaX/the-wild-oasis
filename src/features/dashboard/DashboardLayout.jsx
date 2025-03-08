1478ce3250f489707a52a9e6cd3ffc758e95f828
NidzaX <59400424+NidzaX@users.noreply.github.com>
2025-03-08 07:17:16 +0100

    Computing Recent Bookings and Stays

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,8 +1,30 @@
 import styled from "styled-components";
 
+import Spinner from "../../ui/Spinner";
+
+import { useRecentBookings } from "./useRecentBookings";
+import { useRecentStays } from "./useRecentStays";
+
 const StyledDashboardLayout = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+  const { bookings, isLoading: isLoading1 } = useRecentBookings();
+  const { stays, confirmedStays, isLoading: isLoading2 } = useRecentStays();
+
+  if (isLoading1 || isLoading2) return <Spinner />;
+  return (
+    <StyledDashboardLayout>
+      <div>Statistics</div>
+      <div>Today&apos;s activity</div>
+      <div>Chart stay durations</div>
+      <div>Chart sales</div>
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashboardLayout;
